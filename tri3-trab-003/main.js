1727a8e8f9566764f8536ce4920f7cfdd686fe2f
gatino <gatinocorp@gmail.com>
2024-10-29 11:44:50 -0300

    ...

--- tri3-trab-003/main.js
+++ tri3-trab-003/main.js
@@ -11,3 +11,12 @@ document.querySelectorAll("[data-folder]").forEach(el => {
   }
   el.innerHTML = html
 })
+
+
+const galleryImageModal = document.querySelector('.gallery-image-modal')
+const galleryBtClose = galleryImageModal.querySelector('.bt-close')
+
+galleryImageModal.showModal()
+galleryImageModal.addEventListener('click', () => {
+  galleryImageModal.close()
+})
