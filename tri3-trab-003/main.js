b328c9c247c8d32428741a5612c70bac45a65711
gatino <gatinocorp@gmail.com>
2024-11-06 16:56:44 -0300

    ...

--- tri3-trab-003/main.js
+++ tri3-trab-003/main.js
@@ -26,13 +26,44 @@ const galleryBtClose = galleryImageModal.querySelector('.bt-close')
 const galleryContent = galleryImageModal.querySelector('.content')
 const imgs = document.querySelectorAll('.gallery img')
 
-galleryImageModal.addEventListener('click', () => {
+galleryBtClose.addEventListener('click', () => {
   galleryImageModal.close()
 })
 
-imgs.forEach((img) => {
+imgs.forEach(img => {
   img.addEventListener('click', () => {
-    galleryContent.innerHTML = `<img src="${img.src}" alt="${img.alt}">`
+    galleryContent.innerHTML = `<img src="${img.src}">`
     galleryImageModal.showModal()
   })
 })
+
+// // 
+// // Lógica para abrir e fechar o modal com a imagem // EXEMPLO DE COMO FAZER OS BTS de NEXT e PREV
+// // 
+
+// const galleryImageModal = document.querySelector('.gallery-image-modal')
+// const galleryBtClose = galleryImageModal.querySelector('.bt-close')
+// const galleryContent = galleryImageModal.querySelector('.content')
+// const imgs = document.querySelectorAll('.gallery img')
+
+// // DICASSSS: BEGIN
+// const galleryBtNext = galleryImageModal.querySelector('.bt-next')
+// let nextImg = null
+
+// galleryBtNext.addEventListener('click', () => {
+//   galleryImageModal.close()
+//   nextImg.click()
+// })
+// // DICASSSS: END
+
+// galleryBtClose.addEventListener('click', () => {
+//   galleryImageModal.close()
+// })
+
+// imgs.forEach(img => {
+//   img.addEventListener('click', () => {
+//     nextImg = img.parentNode.nextElementSibling.querySelector('img') // DICASSSS
+//     galleryContent.innerHTML = `<img src="${img.src}">`
+//     galleryImageModal.showModal()
+//   })
+// })
